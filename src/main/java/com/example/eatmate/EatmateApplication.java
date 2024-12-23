5bd1fe715508f960c1f71c43e90bbd39731ed689
EunHyunsu <ehs208@naver.com>
2024-12-23 21:49:43 +0900

    chore: 초기 프로젝트 세팅

---
+++ src/main/java/com/example/eatmate/EatmateApplication.java
@@ -0,0 +1,13 @@
+package com.example.eatmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EatmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EatmateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eatmate/EatmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eatmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EatmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
