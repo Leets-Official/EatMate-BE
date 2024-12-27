b42044f2ba7ee97f1741b0cff4ebde432e888b9e
seokjun01 <suckjungod@naver.com>
2024-12-28 03:14:10 +0900

    [add] #1 최초 로그인 구별하기 위한 GUEST 생성

--- src/main/java/com/example/eatmate/app/domain/member/domain/Role.java
+++ src/main/java/com/example/eatmate/app/domain/member/domain/Role.java
@@ -2,7 +2,8 @@ package com.example.eatmate.app.domain.member.domain;
 
 public enum Role {
     USER("사용자"),
-    ADMIN("관리자");
+    ADMIN("관리자"),
+    GUEST("게스트"); //첫 로그인 구별
 
     private final String RoleType;
 
