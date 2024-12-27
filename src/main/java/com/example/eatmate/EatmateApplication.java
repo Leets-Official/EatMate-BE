9ebcba316846aa8f433cf27457f590e1ed8e261e
seokjun01 <suckjungod@naver.com>
2024-12-28 03:09:07 +0900

    [del] #1 @SpringBootApplication 옆에 옵션 삭제

--- src/main/java/com/example/eatmate/EatmateApplication.java
+++ src/main/java/com/example/eatmate/EatmateApplication.java
@@ -2,9 +2,10 @@ package com.example.eatmate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication
-public class EatmateApplication {
+public class EatmateApplication  {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EatmateApplication.class, args);
