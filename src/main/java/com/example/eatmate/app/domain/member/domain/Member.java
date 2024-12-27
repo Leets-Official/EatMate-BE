b5e617fdfc94812a804245072e491371ec8ef53d
seokjun01 <suckjungod@naver.com>
2024-12-28 03:12:32 +0900

    [feat] #1 회원가입 시, 업데이트할 필드 메서드 정의

--- src/main/java/com/example/eatmate/app/domain/member/domain/Member.java
+++ src/main/java/com/example/eatmate/app/domain/member/domain/Member.java
@@ -1,10 +1,8 @@
 package com.example.eatmate.app.domain.member.domain;
 
+import com.example.eatmate.app.domain.member.dto.MemberSignUpRequestDto;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 @Entity
 @Getter
@@ -17,41 +15,41 @@ public class Member {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long memberId;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String name;
 
-    @Column (nullable = false)
+    @Column (nullable = true)
     private String nickname;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String studentNumber;
 
     @Column(nullable = true)
     private String mbti;
 
-    @Column(nullable = false)
+    @Column(nullable = true)
     private String phoneNumber;
 
-    @Column(nullable = false)
-    private int birthYear;
-
-    @Column(nullable = false)
-    private Boolean isActive = false;
+    @Column(nullable = true)
+    private Boolean isActive = true;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = true)
     private Gender gender;
 
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false)
+    @Column(nullable = true)
     private Role role;
 
     private String refreshToken;
 
-    public Member(Long memberId, String email, String name, String nickname, String studentNumber, String mbti, String phoneNumber, int birthYear, Boolean isActive, Gender gender, Role role) {
+    private BirthDate birthDate;   // yyyy-mm-dd 형식으로 받아야함
+
+
+    public Member(Long memberId, String email, String name, String nickname, String studentNumber, String mbti, String phoneNumber, BirthDate birthDate, Boolean isActive, Gender gender, Role role, String refreshToken) {
         this.memberId = memberId;
         this.email = email;
         this.name = name;
@@ -59,13 +57,55 @@ public class Member {
         this.studentNumber = studentNumber;
         this.mbti = mbti;
         this.phoneNumber = phoneNumber;
-        this.birthYear = birthYear;
+        this.birthDate = birthDate;
         this.isActive = isActive;
         this.gender = gender;
         this.role = role;
+        this.refreshToken = refreshToken;
+    }
+
+    public static Member create(String email, String nickname, String mbti, String phoneNumber, int year, int month, int day, Gender gender, String studentNumber) {
+        // BirthDate 객체 생성
+        BirthDate birthDate = BirthDate.of(year, month, day);
+
+        // Member 객체 생성
+        return Member.builder()
+                .email(email)
+                .nickname(nickname)
+                .mbti(mbti)
+                .phoneNumber(phoneNumber)
+                .birthDate(birthDate) // BirthDate 설정
+                .gender(gender)
+                .studentNumber(studentNumber)
+                .role(Role.USER) // 기본 역할 설정
+                .build();
     }
 
     public void updateRefreshToken(String updateRefreshToken) {
         this.refreshToken = updateRefreshToken;
     }
+
+
+    public void updateNickname(String nickname) {
+        this.nickname = nickname;
+    }
+
+
+    public void updatePhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void updateStudentNumber(String studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+
+    public void updateGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public void activate() {
+        this.isActive = true;
+    }
+
+
 }
