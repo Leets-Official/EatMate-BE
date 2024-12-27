531405824e6d25f35df784de793a71d5297f0dec
seokjun01 <suckjungod@naver.com>
2024-12-28 03:11:02 +0900

    [chore] #1 MemberController 임시 생성

--- src/main/java/com/example/eatmate/app/domain/member/controller/MemberController.java
+++ src/main/java/com/example/eatmate/app/domain/member/controller/MemberController.java
@@ -1,4 +1,17 @@
 package com.example.eatmate.app.domain.member.controller;
 
+import com.example.eatmate.app.domain.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/profile")
+@RequiredArgsConstructor
 public class MemberController {
+
+    //추후 구현 예정
+    private final MemberService memberService;
+
 }
