bca3eea9361f7e8453dc5c44f7527ebba5914910
seokjun01 <suckjungod@naver.com>
2024-12-28 03:17:06 +0900

    [feat] #1 회원가입 시, 필요한 DTO 구현

---
+++ src/main/java/com/example/eatmate/app/domain/member/dto/MemberSignUpRequestDto.java
@@ -0,0 +1,24 @@
+package com.example.eatmate.app.domain.member.dto;
+
+
+import com.example.eatmate.app.domain.member.domain.Gender;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@Getter
+public class MemberSignUpRequestDto {
+    //생년월일, 성별, 전화번호, MBTI, 학번, 닉네임, 프로필 사진 (일단 패스)
+    private String email;
+    private int Year;
+    private int Month;
+    private int Day;
+    private Gender gender;
+    private String phoneNumber;
+    private String mbti;
+    private String StudentNumber;
+    private String nickname;
+
+
+}
