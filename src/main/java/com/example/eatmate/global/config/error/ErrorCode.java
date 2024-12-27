e00192d2644ec9e25fc770512f5d31008ae6454e
seokjun01 <suckjungod@naver.com>
2024-12-28 03:33:25 +0900

    [add] #1 중복 회원, 유효하지 않은 토큰 에러 코드 추가

--- src/main/java/com/example/eatmate/global/config/error/ErrorCode.java
+++ src/main/java/com/example/eatmate/global/config/error/ErrorCode.java
@@ -10,6 +10,12 @@ public enum ErrorCode {
 	//공통
 	INTERNAL_SERVER_ERROR(500, "INTERNAL_SERVER_ERROR", "서버 내부 에러"),
 
+	//중복된 회원
+	MEMBER_ALREADY_EXISTS(409, "MEMBER_ALREADY_EXISTS", "이미 가입된 이메일입니다."),
+
+	//유효하지 않은 토큰
+	INVALID_TOKEN(401, "INVALID_TOKEN",  "유효하지 않은 토큰입니다."),
+
 	//회원
 	USER_NOT_FOUND(404, "USER_NOT_FOUND", "유저를 찾을 수 없습니다.");
 
