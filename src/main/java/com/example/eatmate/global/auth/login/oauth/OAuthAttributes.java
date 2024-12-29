60845365c2f7a083f600d2f3ff8b3cd359ee937f
seokjun01 <suckjungod@naver.com>
2024-12-29 16:33:48 +0900

    [mod] #1 기본 값 상수처리

--- src/main/java/com/example/eatmate/global/auth/login/oauth/OAuthAttributes.java
+++ src/main/java/com/example/eatmate/global/auth/login/oauth/OAuthAttributes.java
@@ -14,6 +14,14 @@ public class OAuthAttributes {
     private final String nameAttributeKey;
     private final GoogleOAuthUserInfo googleOAuthUserInfo;
 
+     // 기본값 처리를 위해 상수 생성
+    private static final String DEFAULT_NAME = "Unnamed";
+    private static final String DEFAULT_NICKNAME = "default_nickname";
+    private static final String DEFAULT_PHONE_NUMBER = "010-0000-0000";
+    private static final String DEFAULT_STUDENT_NUMBER = "default_student";
+    private static final Role DEFAULT_ROLE = Role.GUEST;
+    private static final boolean DEFAULT_IS_ACTIVE = false;
+
     @Builder
     private OAuthAttributes(String nameAttributeKey, GoogleOAuthUserInfo googleOAuthUserInfo) {
         this.nameAttributeKey = nameAttributeKey;
@@ -50,14 +58,13 @@ public class OAuthAttributes {
     public Member toEntity() {
         return Member.builder()
                 .email(getEmail()) // 구글에서 얻어온 이메일
-                .name(getName() != null ? getName() : "Unnamed") // 이름이 없다면 기본값 설정
-                .nickname("default_nickname") // 기본 닉네임 설정
-                .phoneNumber("010-0000-0000") // 기본 전화번호 설정
-                .studentNumber("default_student") // 기본 학번 설정
-                .role(Role.GUEST) // 기본 Role 설정
-                .isActive(false) // 비활성화 상태로 설정
+                .name(getName() != null ? getName() : DEFAULT_NAME) // 이름이 없으면 기본값 사용
+                .nickname(DEFAULT_NICKNAME) // 기본 닉네임 설정
+                .phoneNumber(DEFAULT_PHONE_NUMBER) // 기본 전화번호 설정
+                .studentNumber(DEFAULT_STUDENT_NUMBER) // 기본 학번 설정
+                .role(DEFAULT_ROLE) // 기본 Role 설정
+                .isActive(DEFAULT_IS_ACTIVE) // 비활성화 상태로 설정
                 .build();
     }
-
 
 }
