1c72ae6e8d83108d9113f32c9de702df6ea5f606
EunHyunsu <ehs208@naver.com>
2024-12-31 03:04:41 +0900

    [mod] #3 BaseTimeEntity 직접 구현

--- src/main/java/com/example/eatmate/app/domain/meeting/domain/Meeting.java
+++ src/main/java/com/example/eatmate/app/domain/meeting/domain/Meeting.java
@@ -1,6 +1,10 @@
 package com.example.eatmate.app.domain.meeting.domain;
 
+import java.time.LocalDateTime;
+
 import org.hibernate.validator.constraints.Range;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
 import com.example.eatmate.app.domain.member.domain.Gender;
 import com.example.eatmate.global.common.BaseTimeEntity;
@@ -16,16 +20,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Inheritance;
 import jakarta.persistence.InheritanceType;
 import jakarta.persistence.Id;
-import jakarta.validation.constraints.NotNull;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
 
 @Entity
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Inheritance(strategy = InheritanceType.JOINED)
 @DiscriminatorColumn
-public abstract class Meeting extends BaseTimeEntity {
+@SuperBuilder
+public abstract class Meeting {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -45,6 +53,14 @@ public abstract class Meeting extends BaseTimeEntity {
 	@Range(min = 2, max = 10)
 	@Embedded
 	private ParticipantLimit participantLimit;
+
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column
+	private LocalDateTime updatedAt;
 
 }
 
