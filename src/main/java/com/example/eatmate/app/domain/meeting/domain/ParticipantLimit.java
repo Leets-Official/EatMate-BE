a438e5dab1fea0d5ace11ddc1c01aca826a138c4
EunHyunsu <ehs208@naver.com>
2024-12-31 15:27:28 +0900

    [feat] #3 미팅 참여제한 생성자 생성

--- src/main/java/com/example/eatmate/app/domain/meeting/domain/ParticipantLimit.java
+++ src/main/java/com/example/eatmate/app/domain/meeting/domain/ParticipantLimit.java
@@ -4,12 +4,24 @@ import org.hibernate.validator.constraints.Range;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
 @Embeddable
+@NoArgsConstructor
 public class ParticipantLimit {
+
 	private boolean isUnlimited;
 
 	@Column(nullable = true)
 	@Range(min = 2, max = 10)
 	private Long maxParticipants;
+
+	@Builder
+	private ParticipantLimit(boolean isUnlimited, Long maxParticipants) {
+		this.isUnlimited = isUnlimited;
+		this.maxParticipants = maxParticipants;
+	}
 }
