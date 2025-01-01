2ae004afaf5e752d73dc03d08bcea19f36254863
EunHyunsu <ehs208@naver.com>
2025-01-01 21:51:43 +0900

    [mod] #3 성별 값 변경

--- src/main/java/com/example/eatmate/app/domain/meeting/domain/GenderRestriction.java
+++ src/main/java/com/example/eatmate/app/domain/meeting/domain/GenderRestriction.java
@@ -1,11 +1,12 @@
 package com.example.eatmate.app.domain.meeting.domain;
 
 public enum GenderRestriction {
-	ALL("모두 참여"),
-	SAME_GENDER("같은 성별");
+	MALE("남성"),
+	FEMALE("여성"),
+	ALL("무관"),
+	;
 
 	private final String description;
-
 
 	GenderRestriction(String description) {
 		this.description = description;
