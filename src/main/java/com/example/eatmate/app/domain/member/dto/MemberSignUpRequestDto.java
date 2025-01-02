b2932a57a00e0089b6f47b221b3819362bf4f910
seokjun01 <suckjungod@naver.com>
2025-01-03 01:03:13 +0900

    [del] #5 email 필드 삭제

--- src/main/java/com/example/eatmate/app/domain/member/dto/MemberSignUpRequestDto.java
+++ src/main/java/com/example/eatmate/app/domain/member/dto/MemberSignUpRequestDto.java
@@ -1,23 +1,24 @@
 package com.example.eatmate.app.domain.member.dto;
 
+
 import com.example.eatmate.app.domain.member.domain.Gender;
 import com.example.eatmate.app.domain.member.domain.Mbti;
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
 
 @NoArgsConstructor
 @Getter
 public class MemberSignUpRequestDto {
-	//생년월일, 성별, 전화번호, MBTI, 학번, 닉네임, 프로필 사진 (일단 패스)
-	private String email;
-	private int Year;
-	private int Month;
-	private int Day;
-	private Gender gender;
-	private String phoneNumber;
-	private Mbti mbti;
-	private Long StudentNumber;
-	private String nickname;
+    //생년월일, 성별, 전화번호, MBTI, 학번, 닉네임, 프로필 사진 (일단 패스)
+    private int year;
+    private int month;
+    private int day;
+    private Gender gender;
+    private String phoneNumber;
+    private Mbti mbti;
+    private Long studentNumber;
+    private String nickname;
+
 
 }
