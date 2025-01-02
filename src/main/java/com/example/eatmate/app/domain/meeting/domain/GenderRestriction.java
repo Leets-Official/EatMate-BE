535363f87f07a83b8fb3165b6e135973e4c892c4
EunHyunsu <ehs208@naver.com>
2025-01-02 18:49:22 +0900

    [mod] #3 성별제한 설명 수정

--- src/main/java/com/example/eatmate/app/domain/meeting/domain/GenderRestriction.java
+++ src/main/java/com/example/eatmate/app/domain/meeting/domain/GenderRestriction.java
@@ -1,9 +1,12 @@
 package com.example.eatmate.app.domain.meeting.domain;
 
+import lombok.Getter;
+
+@Getter
 public enum GenderRestriction {
-	MALE("남성"),
-	FEMALE("여성"),
-	ALL("무관"),
+	MALE("남자들끼리만 모여요"),
+	FEMALE("여자들끼리만 모여요"),
+	ALL("아무나 모여요"),
 	;
 
 	private final String description;
