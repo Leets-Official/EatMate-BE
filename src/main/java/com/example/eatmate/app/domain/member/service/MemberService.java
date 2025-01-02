7b8d4f2b42611217a290d103d1600e3d6aaacd74
seokjun01 <suckjungod@naver.com>
2025-01-03 00:53:44 +0900

    [fix] #5 서비스 로직 수정

--- src/main/java/com/example/eatmate/app/domain/member/service/MemberService.java
+++ src/main/java/com/example/eatmate/app/domain/member/service/MemberService.java
@@ -1,62 +1,49 @@
 package com.example.eatmate.app.domain.member.service;
 
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
+import com.example.eatmate.app.domain.member.domain.BirthDate;
 import com.example.eatmate.app.domain.member.domain.Member;
 import com.example.eatmate.app.domain.member.domain.repository.MemberRepository;
 import com.example.eatmate.app.domain.member.dto.MemberSignUpRequestDto;
+import com.example.eatmate.global.auth.jwt.JwtService;
 import com.example.eatmate.global.config.error.ErrorCode;
 import com.example.eatmate.global.config.error.exception.CommonException;
-
+import com.example.eatmate.global.config.error.exception.custom.MemberAlreadyExistsException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class MemberService {
 
-	private final MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
+    private final JwtService jwtService;
 
-	// 회원 가입 완료시 , 기본 값에서 사용자 입력값으로 업데이트 해주는 메소드
-	public void completeRegistration(MemberSignUpRequestDto signUpRequestDto) {
-		// 이메일로 기존 사용자 조회
-		Member member = memberRepository.findByEmail(signUpRequestDto.getEmail())
-			.orElseThrow(() -> new CommonException(ErrorCode.USER_NOT_FOUND));   // 추가 정보 업데이트
-		updateMemberDetails(member, signUpRequestDto);
-		// 변경된 정보 저장
-		memberRepository.save(member);
-	}
+    // 회원 가입 완료시 , 기본 값에서 사용자 입력값으로 업데이트 해주는 메소드
+    public Void completeRegistration(MemberSignUpRequestDto signUpRequestDto, UserDetails userDetails) {
+        String email = userDetails.getUsername();
+        // 이메일로 기존 사용자 조회
+        Member member = memberRepository.findByEmail(email)
+                .orElseThrow(() -> new CommonException(ErrorCode.USER_NOT_FOUND));   // 추가 정보 업데이트
+        member.updateMemberDetails(signUpRequestDto.getNickname(), signUpRequestDto.getPhoneNumber(), signUpRequestDto.getStudentNumber(), signUpRequestDto.getGender(), BirthDate.of(signUpRequestDto.getYear(), signUpRequestDto.getMonth(), signUpRequestDto.getDay()), signUpRequestDto.getMbti());
+        return null;
+    }
 
-	public String register(MemberSignUpRequestDto memberSignUpRequestDto) {
-		if (memberRepository.findByEmail(memberSignUpRequestDto.getEmail()).isPresent()) {
-			throw new CommonException(ErrorCode.MEMBER_ALREADY_EXISTS);
-		}
+//    private void updateMemberDetails(Member member, MemberSignUpRequestDto signUpRequestDto) {
+//        member.updateNickname(signUpRequestDto.getNickname());
+//        member.updatePhoneNumber(signUpRequestDto.getPhoneNumber());
+//        member.updateStudentNumber(signUpRequestDto.getStudentNumber());
+//        member.updateGender(signUpRequestDto.getGender());
+//        member.updateMbti(signUpRequestDto.getMbti());
+//        member.updateBirthDate(BirthDate.of(signUpRequestDto.getYear(), signUpRequestDto.getMonth(), signUpRequestDto.getDay()));
+//        member.activate();
+//
+//    }
 
-		Member member = Member.create(
-			memberSignUpRequestDto.getEmail(),
-			memberSignUpRequestDto.getNickname(),
-			memberSignUpRequestDto.getMbti(),
-			memberSignUpRequestDto.getPhoneNumber(),
-			memberSignUpRequestDto.getYear(),
-			memberSignUpRequestDto.getMonth(),
-			memberSignUpRequestDto.getDay(),
-			memberSignUpRequestDto.getGender(),
-			memberSignUpRequestDto.getStudentNumber()
-		);
-
-		memberRepository.save(member);
-
-		return "회원가입이 완료되었습니다.";
-	}
-
-	private void updateMemberDetails(Member member, MemberSignUpRequestDto signUpRequestDto) {
-		member.updateNickname(signUpRequestDto.getNickname());
-		member.updatePhoneNumber(signUpRequestDto.getPhoneNumber());
-		member.updateStudentNumber(signUpRequestDto.getStudentNumber());
-		member.updateGender(signUpRequestDto.getGender());
-		member.activate();
-	}
 
 }
 
