097ca8f0f30fcb534e865883293c3fa156a4bc11
seokjun01 <suckjungod@naver.com>
2025-01-03 05:02:20 +0900

    [feat] #5 닉네임 찾는 메서드 추가

--- src/main/java/com/example/eatmate/app/domain/member/domain/repository/MemberRepository.java
+++ src/main/java/com/example/eatmate/app/domain/member/domain/repository/MemberRepository.java
@@ -1,14 +1,13 @@
 package com.example.eatmate.app.domain.member.domain.repository;
 
-import java.util.Optional;
-
+import com.example.eatmate.app.domain.member.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import com.example.eatmate.app.domain.member.domain.Member;
+import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-	Optional<Member> findByEmail(String email);
-
-	Optional<Member> findByRefreshToken(String refreshToken);
+    Optional<Member> findByEmail(String email);
+    Optional<Member> findByRefreshToken(String refreshToken);
+    boolean existsByNickname(String nickname);
 }
 
