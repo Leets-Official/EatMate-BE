e9d848331910276ca8056d00555d488e4a5e8767
EunHyunsu <ehs208@naver.com>
2025-01-04 01:50:46 +0900

    [feat] #9 개발용 임시 로그인/회원가입 구현

--- src/main/java/com/example/eatmate/app/domain/member/controller/MemberController.java
+++ src/main/java/com/example/eatmate/app/domain/member/controller/MemberController.java
@@ -1,10 +1,15 @@
 package com.example.eatmate.app.domain.member.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.eatmate.app.domain.member.dto.MemberLoginRequestDto;
+import com.example.eatmate.app.domain.member.dto.MemberLoginResponseDto;
 import com.example.eatmate.app.domain.member.service.MemberService;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -15,4 +20,11 @@ public class MemberController {
 	//추후 구현 예정
 	private final MemberService memberService;
 
+	//개발용 임시 로그인/회원가입
+	@PostMapping("/login")
+	@Operation(summary = "로그인", description = "개발전용 임시 로그인/회원가입 API")
+	public MemberLoginResponseDto login(@RequestBody MemberLoginRequestDto memberLoginRequestDto) {
+		return memberService.login(memberLoginRequestDto);
+
+	}
 }
--- src/main/java/com/example/eatmate/app/domain/member/domain/Member.java
+++ src/main/java/com/example/eatmate/app/domain/member/domain/Member.java
@@ -1,137 +1,156 @@
 package com.example.eatmate.app.domain.member.domain;
 
+import static com.example.eatmate.app.domain.member.domain.Role.*;
 
-import jakarta.persistence.*;
-import lombok.*;
 import com.example.eatmate.global.common.BaseTimeEntity;
 
-import static com.example.eatmate.app.domain.member.domain.Role.USER;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Member extends BaseTimeEntity {
-//OAuth로 받을 수 있는거만 false 해놓기
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long memberId;
+	//OAuth로 받을 수 있는거만 false 해놓기
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long memberId;
 
-    @Column(nullable = true)
-    private String email;
+	@Column(nullable = true)
+	private String email;
 
-    @Column(nullable = true)
-    private String name;
+	@Column(nullable = true)
+	private String name;
 
-    @Column (nullable = true)
-    private String nickname;
+	@Column(nullable = true)
+	private String nickname;
 
-    @Column(nullable = true, name = "student_number")
-    private Long studentNumber;
+	@Column(nullable = true, name = "student_number")
+	private Long studentNumber;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = true)
-    private Mbti mbti;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = true)
+	private Mbti mbti;
 
-    @Column(nullable = true)
-    private String phoneNumber;
+	@Column(nullable = true)
+	private String phoneNumber;
 
-    @Column(nullable = true)
-    private Boolean isActive = true;
+	@Column(nullable = true)
+	private Boolean isActive = true;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = true)
-    private Gender gender;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = true)
+	private Gender gender;
 
-    @Enumerated(EnumType.STRING)
-    @Column(nullable = true)
-    private Role role;
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = true)
+	private Role role;
 
-    @Column(name = "refresh_token")
-    private String refreshToken;
+	@Column(name = "refresh_token")
+	private String refreshToken;
 
-    @Embedded
-    private BirthDate birthDate;   // yyyy-mm-dd 형식으로 받아야함
+	@Embedded
+	private BirthDate birthDate;   // yyyy-mm-dd 형식으로 받아야함
 
-    @Builder
-    private Member(Long memberId, String email, String name, String nickname, Long studentNumber, Mbti mbti, String phoneNumber, BirthDate birthDate, Boolean isActive, Gender gender, Role role, String refreshToken) {
-        this.memberId = memberId;
-        this.email = email;
-        this.name = name;
-        this.nickname = nickname;
-        this.studentNumber = studentNumber;
-        this.mbti = mbti;
-        this.phoneNumber = phoneNumber;
-        this.birthDate = birthDate;
-        this.isActive = isActive;
-        this.gender = gender;
-        this.role = role;
-        this.refreshToken = refreshToken;
-    }
+	@Builder
+	private Member(Long memberId, String email, String name, String nickname, Long studentNumber, Mbti mbti,
+		String phoneNumber, BirthDate birthDate, Boolean isActive, Gender gender, Role role, String refreshToken) {
+		this.memberId = memberId;
+		this.email = email;
+		this.name = name;
+		this.nickname = nickname;
+		this.studentNumber = studentNumber;
+		this.mbti = mbti;
+		this.phoneNumber = phoneNumber;
+		this.birthDate = birthDate;
+		this.isActive = isActive;
+		this.gender = gender;
+		this.role = role;
+		this.refreshToken = refreshToken;
+	}
 
-    public static Member create(String email, String nickname, Mbti mbti, String phoneNumber, int year, int month, int day, Gender gender, Long studentNumber) {
-        // BirthDate 객체 생성
-        BirthDate birthDate = BirthDate.of(year, month, day);
+	private static Member create(String email, String nickname, Mbti mbti, String phoneNumber, int year, int month,
+		int day, Gender gender, Long studentNumber) {
+		// BirthDate 객체 생성
+		BirthDate birthDate = BirthDate.of(year, month, day);
 
+		// Member 객체 생성
+		return Member.builder()
+			.email(email)
+			.nickname(nickname)
+			.mbti(mbti)
+			.phoneNumber(phoneNumber)
+			.birthDate(birthDate) // BirthDate 설정
+			.gender(gender)
+			.studentNumber(studentNumber)
+			.role(USER) // 기본 역할 설정
+			.build();
+	}
 
-        // Member 객체 생성
-        return Member.builder()
-                .email(email)
-                .nickname(nickname)
-                .mbti(mbti)
-                .phoneNumber(phoneNumber)
-                .birthDate(birthDate) // BirthDate 설정
-                .gender(gender)
-                .studentNumber(studentNumber)
-                .role(USER) // 기본 역할 설정
-                .build();
-    }
+	public static Member createDevMember(String email, String nickname, String name, Gender gender) {
+		return Member.builder()
+			.email(email)
+			.name(name)
+			.gender(gender)
+			.nickname(nickname)
+			.build();
+	}
 
-    public void updateMemberDetails(String nickname, String phoneNumber, Long studentNumber, Gender gender, BirthDate birthDate, Mbti mbti) {
-        this.nickname = nickname;
-        this.phoneNumber = phoneNumber;
-        this.studentNumber = studentNumber;
-        this.gender = gender;
-        this.role = USER;
-        this.birthDate = birthDate;
-        this.isActive = true;
-        this.mbti = mbti;
-    }
+	public void updateMemberDetails(String nickname, String phoneNumber, Long studentNumber, Gender
+		gender, BirthDate birthDate, Mbti mbti) {
+		this.nickname = nickname;
+		this.phoneNumber = phoneNumber;
+		this.studentNumber = studentNumber;
+		this.gender = gender;
+		this.role = USER;
+		this.birthDate = birthDate;
+		this.isActive = true;
+		this.mbti = mbti;
+	}
 
-    public void updateRefreshToken(String updateRefreshToken) {
-        this.refreshToken = updateRefreshToken;
-    }
-//
-//
-//    public void updateNickname(String nickname) {
-//        this.nickname = nickname;
-//    }
-//
-//
-//    public void updatePhoneNumber(String phoneNumber) {
-//        this.phoneNumber = phoneNumber;
-//    }
-//
-//    public void updateStudentNumber(Long studentNumber) {
-//        this.studentNumber = studentNumber;
-//    }
-//
-//    public void updateGender(Gender gender) {
-//        this.gender = gender;
-//    }
-//
-//    public void updateBirthDate(BirthDate birthDate) {
-//        this.birthDate = birthDate;
-//    }
-//
-//    public void updateMbti(Mbti mbti) {
-//        this.mbti = mbti;
-//    }
-//
-//    public void activate() {
-//        this.isActive = true;
-//        this.role = Role.USER;
-//    }
-
+	public void updateRefreshToken(String updateRefreshToken) {
+		this.refreshToken = updateRefreshToken;
+	}
+	//
+	//
+	//    public void updateNickname(String nickname) {
+	//        this.nickname = nickname;
+	//    }
+	//
+	//
+	//    public void updatePhoneNumber(String phoneNumber) {
+	//        this.phoneNumber = phoneNumber;
+	//    }
+	//
+	//    public void updateStudentNumber(Long studentNumber) {
+	//        this.studentNumber = studentNumber;
+	//    }
+	//
+	//    public void updateGender(Gender gender) {
+	//        this.gender = gender;
+	//    }
+	//
+	//    public void updateBirthDate(BirthDate birthDate) {
+	//        this.birthDate = birthDate;
+	//    }
+	//
+	//    public void updateMbti(Mbti mbti) {
+	//        this.mbti = mbti;
+	//    }
+	//
+	//    public void activate() {
+	//        this.isActive = true;
+	//        this.role = Role.USER;
+	//    }
 
 }
