224866d731bd077d49a293b58365818b6d86ef6d
seokjun01 <suckjungod@naver.com>
2025-01-03 18:11:11 +0900

    [mod] #5 역직렬화 오류 수정

--- src/main/java/com/example/eatmate/app/domain/member/domain/Mbti.java
+++ src/main/java/com/example/eatmate/app/domain/member/domain/Mbti.java
@@ -1,20 +1,22 @@
 package com.example.eatmate.app.domain.member.domain;
 
+import com.example.eatmate.global.config.error.ErrorCode;
 import com.example.eatmate.global.config.error.exception.CommonException;
-import com.example.eatmate.global.config.error.exception.custom.InvalidMbtiException;
+import com.fasterxml.jackson.annotation.JsonCreator;
 
 public enum Mbti {
-	ISTJ, ISFJ, INFJ, INTJ,
-	ISTP, ISFP, INFP, INTP,
-	ESTP, ESFP, ENFP, ENTP,
-	ESTJ, ESFJ, ENFJ, ENTJ;
+    ISTJ, ISFJ, INFJ, INTJ,
+    ISTP, ISFP, INFP, INTP,
+    ESTP, ESFP, ENFP, ENTP,
+    ESTJ, ESFJ, ENFJ, ENTJ;
 
-	public static Mbti fromString(String mbti) {
+    @JsonCreator
+    public static Mbti fromString(String mbti) {
 
-		try {
-			return Mbti.valueOf(mbti.toUpperCase());
-		} catch (CommonException e) {
-			throw new InvalidMbtiException();
-		}
-	}
+        try {
+            return Mbti.valueOf(mbti.toUpperCase().trim());
+        } catch (IllegalArgumentException e) {
+            throw new CommonException(ErrorCode.INVALID_MBTI);
+        }
+    }
 }
