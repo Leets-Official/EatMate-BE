3d48c4c765c106b2fdd2a0f9b73dbcf7e83a5a3b
jj <syeonie0526@gmail.com>
2025-01-06 00:22:33 +0900

    [refactor] #4 ADMIN인지 확인할 수 있도록 추가

--- src/main/java/com/example/eatmate/global/config/SecurityConfig.java
+++ src/main/java/com/example/eatmate/global/config/SecurityConfig.java
@@ -1,15 +1,12 @@
 package com.example.eatmate.global.config;
 
-import com.example.eatmate.global.auth.login.oauth.CustomOAuth2UserService;
-import com.example.eatmate.global.auth.login.oauth.OAuthLoginFailureHandler;
-import com.example.eatmate.global.auth.login.oauth.OAuthLoginSuccessHandler;
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
@@ -19,11 +16,15 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
+import com.example.eatmate.global.auth.login.oauth.OAuthLoginFailureHandler;
+import com.example.eatmate.global.auth.login.oauth.OAuthLoginSuccessHandler;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig {
 
 	private final OAuthLoginSuccessHandler oAuthLoginSuccessHandler;
@@ -32,23 +33,24 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-				.csrf(AbstractHttpConfigurer::disable)
-				.cors(Customizer.withDefaults())
-				.headers(headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-				.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authorizeHttpRequests(
-						authorize -> authorize
-						// 아이콘, css, js 관련
+			.csrf(AbstractHttpConfigurer::disable)
+			.cors(Customizer.withDefaults())
+			.headers(
+				headersConfigurer -> headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
+			.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+			.authorizeHttpRequests(
+				authorize -> authorize
+					// 아이콘, css, js 관련
 					// 기본 페이지, css, image, js 하위 폴더에 있는 자료들은 모두 접근 가능, h2-console에 접근 가능
 					// 			.requestMatchers("/", "/css/**", "/images/**", "/js/**", "/favicon.ico").permitAll()
 					// 			.requestMatchers("/v3/api-docs", "/v3/api-docs/", "/swagger-ui.html", "/swagger-ui/", "/swagger/**").permitAll()
 					// 			.requestMatchers("/register").permitAll()
-								.anyRequest().permitAll()
-				)
-				//== 소셜 로그인 설정 ==//
-				.oauth2Login(oauth2 -> oauth2.successHandler(oAuthLoginSuccessHandler)
+					.anyRequest().permitAll()
+			)
+			//== 소셜 로그인 설정 ==//
+			.oauth2Login(oauth2 -> oauth2.successHandler(oAuthLoginSuccessHandler)
 				.failureHandler(oAuthLoginFailureHandler)); // 소셜 로그인 실패 시 핸들러 설정
-				//.userInfoEndpoint().userService(customOAuth2UserService)); // customUserService 설정
+		//.userInfoEndpoint().userService(customOAuth2UserService)); // customUserService 설정
 
 		return http.build();
 	}
