8249e048355cfa95daafa99224708417edeb9a22
jj <syeonie0526@gmail.com>
2025-01-06 21:13:02 +0900

    [refactor] #4 ReportResponseDto에 @AllArgsConstructor 추가 및 변수명 변경

--- src/main/java/com/example/eatmate/app/domain/report/dto/ReportResponseDto.java
+++ src/main/java/com/example/eatmate/app/domain/report/dto/ReportResponseDto.java
@@ -5,22 +5,15 @@ import java.util.List;
 
 import com.example.eatmate.app.domain.report.domain.ReportType;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
+@AllArgsConstructor
 @Getter
 public class ReportResponseDto {
 	private String reportedUserName;
 	private List<ReportType> reportTypes;
 	private String reportingReasonDescription;
 	private LocalDateTime time;
-	private boolean processed;
-
-	public ReportResponseDto(String reportedUserName, List<ReportType> reportTypes,
-		String reportingReasonDescription, LocalDateTime time, boolean processed) {
-		this.reportedUserName = reportedUserName;
-		this.reportTypes = reportTypes;
-		this.reportingReasonDescription = reportingReasonDescription;
-		this.time = time;
-		this.processed = processed;
-	}
+	private boolean isProcessed;
 }
