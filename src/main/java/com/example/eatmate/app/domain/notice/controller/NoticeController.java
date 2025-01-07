02dad572207748717b73f85e1a7cc7f5744e5bcd
jj <syeonie0526@gmail.com>
2025-01-08 00:38:24 +0900

    [feat] #17 단일 공지사항 조회 기능 추가

---
+++ src/main/java/com/example/eatmate/app/domain/notice/controller/NoticeController.java
@@ -0,0 +1,31 @@
+package com.example.eatmate.app.domain.notice.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.eatmate.app.domain.notice.dto.NoticeResponseDto;
+import com.example.eatmate.app.domain.notice.service.NoticeService;
+import com.example.eatmate.global.response.GlobalResponseDto;
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/notices")
+@RequiredArgsConstructor
+public class NoticeController {
+
+	private final NoticeService noticeService;
+
+	@GetMapping("/{noticeId}")
+	@Operation(summary = "단일 공지사항 조회", description = "단일 공지사항을 조회합니다.")
+	public ResponseEntity<GlobalResponseDto<NoticeResponseDto>> getNotice(@PathVariable Long noticeId) {
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(GlobalResponseDto.success(noticeService.findNotice(noticeId)));
+	}
+
+}
--- src/main/java/com/example/eatmate/app/domain/notice/service/NoticeService.java
+++ src/main/java/com/example/eatmate/app/domain/notice/service/NoticeService.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Service;
 import com.example.eatmate.app.domain.notice.domain.Notice;
 import com.example.eatmate.app.domain.notice.domain.repository.NoticeRepository;
 import com.example.eatmate.app.domain.notice.dto.NoticeAdminRequestDto;
+import com.example.eatmate.app.domain.notice.dto.NoticeResponseDto;
+import com.example.eatmate.global.config.error.ErrorCode;
+import com.example.eatmate.global.config.error.exception.CommonException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -17,5 +20,12 @@ public class NoticeService {
 	public void createNotice(NoticeAdminRequestDto noticeAdminRequestDto) {
 		Notice notice = Notice.createNotice(noticeAdminRequestDto);
 		noticeRepository.save(notice);
+	}
+
+	public NoticeResponseDto findNotice(Long noticeId) {
+		Notice notice = noticeRepository.findById(noticeId)
+			.orElseThrow(() -> new CommonException(ErrorCode.NOTICE_NOT_FOUND));
+
+		return NoticeResponseDto.createNoticeResponseDto(notice);
 	}
 }
