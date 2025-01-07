6e173181ec7326e2fdc83e7203fda747684975f9
jj <syeonie0526@gmail.com>
2025-01-08 00:20:10 +0900

    [feat] #17 Notice 엔티티와 dto 생성

---
+++ src/main/java/com/example/eatmate/app/domain/notice/domain/Notice.java
@@ -0,0 +1,41 @@
+package com.example.eatmate.app.domain.notice.domain;
+
+import com.example.eatmate.app.domain.notice.dto.NoticeAdminRequestDto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Getter
+public class Notice {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String title;
+
+	@Column(columnDefinition = "TEXT")
+	private String content;
+
+	@Builder
+	private Notice(NoticeAdminRequestDto noticeAdminRequestDto) {
+		this.title = noticeAdminRequestDto.getTitle();
+		this.content = noticeAdminRequestDto.getContent();
+	}
+
+	public static Notice createNotice(NoticeAdminRequestDto noticeAdminRequestDto) {
+		return Notice.builder()
+			.noticeAdminRequestDto(noticeAdminRequestDto)
+			.build();
+	}
+
+}
