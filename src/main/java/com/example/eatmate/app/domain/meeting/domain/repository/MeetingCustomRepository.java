d8d65fbeeb2160176b739477060d0bc4e1072ed8
EunHyunsu <ehs208@naver.com>
2025-01-07 21:32:52 +0900

    Revert "[feat] #15 QueryDSL 설정"
    This reverts commit 73f510ac732f7ce9fe043b650c37e965ea323b4d.

--- src/main/java/com/example/eatmate/app/domain/meeting/domain/repository/MeetingCustomRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.eatmate.app.domain.meeting.domain.repository;
-
-import java.util.List;
-
-import com.example.eatmate.app.domain.meeting.dto.CreatedMeetingListResponseDto;
-
-public interface MeetingCustomRepository {
-	List<CreatedMeetingListResponseDto> findAllHostMeetings(Long memberId);
-}
