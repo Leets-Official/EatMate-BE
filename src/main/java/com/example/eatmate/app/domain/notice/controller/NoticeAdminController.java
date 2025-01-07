002cd53476594c7eebdb7ada9702df9058333dd8
jj <syeonie0526@gmail.com>
2025-01-08 01:24:45 +0900

    [feat] #17 공지사항 수정 기능 추가

--- src/main/java/com/example/eatmate/app/domain/notice/controller/NoticeAdminController.java
+++ src/main/java/com/example/eatmate/app/domain/notice/controller/NoticeAdminController.java
@@ -2,6 +2,8 @@ package com.example.eatmate.app.domain.notice.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,6 +34,17 @@ public class NoticeAdminController {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(GlobalResponseDto.success());
 
+	}
+
+	@PatchMapping("/{noticeId}")
+	@Operation(summary = "관리자 공지사항 수정", description = "관리자가 공지사항을 수정합니다.")
+	public ResponseEntity<GlobalResponseDto<Void>> update(@PathVariable Long noticeId,
+		@RequestBody @Valid NoticeAdminRequestDto noticeAdminRequestDto) {
+
+		noticeService.updateNotice(noticeId, noticeAdminRequestDto);
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(GlobalResponseDto.success());
 	}
 
 }
