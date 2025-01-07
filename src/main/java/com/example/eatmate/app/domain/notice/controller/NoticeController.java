8100c36913ffffceea40dd38b2f609a84a9b02a4
jj <syeonie0526@gmail.com>
2025-01-08 01:13:57 +0900

    [feat] #17 공지사항 무한스크롤 형태로 조회

--- src/main/java/com/example/eatmate/app/domain/notice/controller/NoticeController.java
+++ src/main/java/com/example/eatmate/app/domain/notice/controller/NoticeController.java
@@ -1,10 +1,12 @@
 package com.example.eatmate.app.domain.notice.controller;
 
+import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.eatmate.app.domain.notice.dto.NoticeResponseDto;
@@ -26,6 +28,16 @@ public class NoticeController {
 	public ResponseEntity<GlobalResponseDto<NoticeResponseDto>> getNotice(@PathVariable Long noticeId) {
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(GlobalResponseDto.success(noticeService.findNotice(noticeId)));
+	}
+
+	@GetMapping
+	@Operation(summary = "공지사항 목록 조회", description = "공지사항을 조회합니다.")
+	public ResponseEntity<GlobalResponseDto<Slice<NoticeResponseDto>>> getNotices(
+		@RequestParam int pageNumber,
+		@RequestParam int pageSize) {
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(GlobalResponseDto.success(noticeService.findNotices(pageNumber, pageSize)));
 	}
 
 }
