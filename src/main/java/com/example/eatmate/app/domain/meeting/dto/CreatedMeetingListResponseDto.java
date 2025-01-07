32d9a0a29042e4d5b50308fb598988e597c130dc
EunHyunsu <ehs208@naver.com>
2025-01-07 21:52:27 +0900

    Reapply "[feat] #15 내가 생성한 목록 조회"
    This reverts commit 02e934e200a6da9f55a48a347cc3bcbf0fb81636.

---
+++ src/main/java/com/example/eatmate/app/domain/meeting/dto/CreatedMeetingListResponseDto.java
@@ -0,0 +1,43 @@
+package com.example.eatmate.app.domain.meeting.dto;
+
+import java.time.LocalDateTime;
+
+import com.example.eatmate.app.domain.meeting.domain.MeetingStatus;
+
+import lombok.Getter;
+
+@Getter
+public class CreatedMeetingListResponseDto {
+	private String meetingType;
+	private Long id;
+	private String meetingName;
+	private MeetingStatus meetingStatus;
+	private Long participantCount;
+	private String storeName;    // DeliveryMeeting인 경우
+	private String meetingPlace; // OfflineMeeting인 경우
+	private LocalDateTime orderDeadline;  // DeliveryMeeting인 경우
+	private LocalDateTime meetingDate; // OfflineMeeting인 경우
+
+	public CreatedMeetingListResponseDto(
+		String type,
+		Long id,
+		String meetingName,
+		MeetingStatus meetingStatus,
+		String storeName,
+		String meetingPlace,
+		LocalDateTime orderDeadline,
+		LocalDateTime meetingDate,
+		Long participantCount
+	) {
+		this.meetingType = type;
+		this.id = id;
+		this.meetingName = meetingName;
+		this.meetingStatus = meetingStatus;
+		this.storeName = storeName;
+		this.meetingPlace = meetingPlace;
+		this.orderDeadline = orderDeadline;
+		this.meetingDate = meetingDate;
+		this.participantCount = participantCount;
+	}
+}
+
