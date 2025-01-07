4440716cc998ddac40452f8ae9a4d226a5284832
EunHyunsu <ehs208@naver.com>
2025-01-07 09:31:57 +0900

    [feat] #15 모임 조회시 상태 확인

--- src/main/java/com/example/eatmate/app/domain/meeting/domain/repository/OfflineMeetingRepository.java
+++ src/main/java/com/example/eatmate/app/domain/meeting/domain/repository/OfflineMeetingRepository.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.eatmate.app.domain.meeting.domain.MeetingStatus;
 import com.example.eatmate.app.domain.meeting.domain.OfflineMeeting;
 import com.example.eatmate.app.domain.meeting.domain.OfflineMeetingCategory;
 
 public interface OfflineMeetingRepository extends JpaRepository<OfflineMeeting, Long> {
-	List<OfflineMeeting> findAllByOfflineMeetingCategory(OfflineMeetingCategory offlineMeetingCategory);
+	List<OfflineMeeting> findAllByOfflineMeetingCategoryAndMeetingStatus(OfflineMeetingCategory offlineMeetingCategory,
+		MeetingStatus meetingStatus);
 }
