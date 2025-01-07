50af535a48264b0c5b80cd19a531d073a749dfd3
EunHyunsu <ehs208@naver.com>
2025-01-07 21:32:49 +0900

    Revert "[rename] #15 DTO 파일명 수정"
    This reverts commit c69ebea041c7998a87be2545acdd39ad88e21b33.

--- src/main/java/com/example/eatmate/app/domain/meeting/domain/repository/MeetingCustomRepository.java
+++ src/main/java/com/example/eatmate/app/domain/meeting/domain/repository/MeetingCustomRepository.java
@@ -3,8 +3,8 @@ package com.example.eatmate.app.domain.meeting.domain.repository;
 import java.util.List;
 
 import com.example.eatmate.app.domain.meeting.domain.ParticipantRole;
-import com.example.eatmate.app.domain.meeting.dto.MeetingListResponseDto;
+import com.example.eatmate.app.domain.meeting.dto.CreatedMeetingListResponseDto;
 
 public interface MeetingCustomRepository {
-	List<MeetingListResponseDto> findAllMeetings(Long memberId, ParticipantRole role);
+	List<CreatedMeetingListResponseDto> findAllMeetings(Long memberId, ParticipantRole role);
 }
