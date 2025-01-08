cd32659820fe4c7e90357d90088872c5079a0248
seokjun01 <suckjungod@naver.com>
2025-01-09 02:46:42 +0900

    [feat] #23 사용자 정보 응답Dto 생성

---
+++ src/main/java/com/example/eatmate/global/auth/login/dto/UserLoginResponseDto.java
@@ -0,0 +1,15 @@
+package com.example.eatmate.global.auth.login.dto;
+
+import com.example.eatmate.app.domain.member.domain.Role;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginResponseDto {
+
+    private String email;
+    private Role role;
+}
