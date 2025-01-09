4ed6059b8eb1757cdc9088dfce476dfbdd4f6f8d
Dongyun Kim <djskaldrjfj@naver.com>
2025-01-10 08:04:44 +0900

    feat #7 채팅방리스폰스

---
+++ src/main/java/com/example/eatmate/app/domain/chatRoom/dto/response/ChatRoomResponseDto.java
@@ -0,0 +1,38 @@
+package com.example.eatmate.app.domain.chatRoom.dto.response;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+
+import com.example.eatmate.app.domain.chat.dto.ChatDto;
+import com.example.eatmate.app.domain.chatRoom.domain.ChatRoom;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class ChatRoomResponseDto {
+	private Page<ChatDto> chats;
+	private List<ChatMemberDto> participants;
+
+	@Builder
+	private ChatRoomResponseDto(Page<ChatDto> chats, List<ChatMemberDto> participants) {
+		this.chats = chats;
+		this.participants = participants;
+	}
+
+	public static ChatRoomResponseDto from(ChatRoom chatRoom, Page<ChatDto> chatPage) {
+		List<ChatMemberDto> participants = chatRoom.getParticipant() != null ? chatRoom.getParticipant()
+			.stream()
+			.map(memberChatRoom -> ChatMemberDto.from(memberChatRoom.getMember()))
+			.collect(Collectors.toList()) : null;
+
+		return ChatRoomResponseDto.builder()
+			.chats(chatPage)
+			.participants(participants)
+			.build();
+	}
+}
