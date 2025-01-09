c3759b2f50676f980660f2e56b916f02c6e0c4e7
Dongyun Kim <djskaldrjfj@naver.com>
2025-01-10 08:18:46 +0900

    [feat] #7 채팅 레포지토리 구현

---
+++ src/main/java/com/example/eatmate/app/domain/chat/domain/repository/ChatRepository.java
@@ -0,0 +1,18 @@
+package com.example.eatmate.app.domain.chat.domain.repository;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.eatmate.app.domain.chat.domain.Chat;
+import com.example.eatmate.app.domain.chatRoom.domain.ChatRoom;
+import com.example.eatmate.global.common.DeletedStatus;
+
+public interface ChatRepository extends JpaRepository<Chat, Long> {
+
+	Page<Chat> findChatByChatRoom(ChatRoom chatRoom, Pageable pageable);
+
+	List<Chat> findChatByChatRoomAndDeletedStatusNot(ChatRoom chatRoom, DeletedStatus deletedStatus);
+}
