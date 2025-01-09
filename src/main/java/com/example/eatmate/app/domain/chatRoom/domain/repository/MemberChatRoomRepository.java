c139407c457902778a29837a2d0b82f60ad85214
Dongyun Kim <djskaldrjfj@naver.com>
2025-01-10 08:01:16 +0900

    feat #7 멤버 중간테이블 레포지토리 구현

---
+++ src/main/java/com/example/eatmate/app/domain/chatRoom/domain/repository/MemberChatRoomRepository.java
@@ -0,0 +1,11 @@
+package com.example.eatmate.app.domain.chatRoom.domain.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.eatmate.app.domain.chatRoom.domain.MemberChatRoom;
+
+public interface MemberChatRoomRepository extends JpaRepository<MemberChatRoom, Long> {
+	Optional<MemberChatRoom> findByMember_MemberId(Long memberId);
+}
