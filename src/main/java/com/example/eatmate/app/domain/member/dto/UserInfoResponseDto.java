27d60eb0614ee4fb779f29364c2c2f3d04501480
seokjun01 <suckjungod@naver.com>
2025-01-10 01:55:27 +0900

    [add] #14 상대방 프로필 조회 Dto (생성만)

---
+++ src/main/java/com/example/eatmate/app/domain/member/dto/UserInfoResponseDto.java
@@ -0,0 +1,13 @@
+package com.example.eatmate.app.domain.member.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class UserInfoResponseDto {
+    private final String userNickname;
+    // 프로필 사진
+    // 차단 여부
+    // 레벨
+}
