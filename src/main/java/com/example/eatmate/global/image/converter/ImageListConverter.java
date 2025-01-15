8780c856c25a1f0c16e6662a693bf0f7c4624f2b
jj <syeonie0526@gmail.com>
2025-01-15 20:56:37 +0900

    [feat] #29 이미지 파일 업로드를 위한 S3 버킷 연결

---
+++ src/main/java/com/example/eatmate/global/image/converter/ImageListConverter.java
@@ -0,0 +1,43 @@
+package com.example.eatmate.global.image.converter;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import jakarta.persistence.AttributeConverter;
+
+public class ImageListConverter implements AttributeConverter<List<String>, String> {
+	private static final ObjectMapper mapper = new ObjectMapper()
+		.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+		.configure(DeserializationFeature.FAIL_ON_NULL_FOR_PRIMITIVES, false);
+
+	@Override
+	public String convertToDatabaseColumn(List<String> attribute) {
+		if (attribute == null || attribute.isEmpty()) {
+			return "";
+		}
+		try {
+			return mapper.writeValueAsString(attribute);
+		} catch (JsonProcessingException e) {
+			throw new IllegalArgumentException();
+		}
+	}
+
+	@Override
+	public List<String> convertToEntityAttribute(String dbData) {
+		if (dbData == null || dbData.isEmpty()) {
+			return new ArrayList<>(); // Return an empty list if dbData is null or empty
+		}
+		try {
+			return mapper.readValue(dbData, new TypeReference<>() {
+			});
+		} catch (IOException e) {
+			throw new IllegalArgumentException();
+		}
+	}
+}
