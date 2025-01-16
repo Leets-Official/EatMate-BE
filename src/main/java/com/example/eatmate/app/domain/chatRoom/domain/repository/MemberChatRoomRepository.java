f6f67d14269bd637f2ce4a80e99b02db3cc3ffdd
Dongyun Kim <djskaldrjfj@naver.com>
2025-01-16 15:56:43 +0900

    [style] #7 eol 해결

--- src/main/java/com/example/eatmate/app/domain/chatRoom/domain/repository/MemberChatRoomRepository.java
+++ src/main/java/com/example/eatmate/app/domain/chatRoom/domain/repository/MemberChatRoomRepository.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.example.eatmate.app.domain.chatRoom.domain.MemberChatRoom;
 
 public interface MemberChatRoomRepository extends JpaRepository<MemberChatRoom, Long> {
+
 	Optional<MemberChatRoom> findByMember_MemberId(Long memberId);
 }
