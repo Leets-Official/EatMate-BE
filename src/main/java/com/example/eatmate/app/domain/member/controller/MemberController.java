ab4ab6b62fdc8904031dd2ee096134bf2decfdad
seokjun01 <suckjungod@naver.com>
2025-01-16 18:17:38 +0900

    [mod]#14 응답 반복 부분 제거

--- src/main/java/com/example/eatmate/app/domain/member/controller/MemberController.java
+++ src/main/java/com/example/eatmate/app/domain/member/controller/MemberController.java
@@ -1,20 +1,24 @@
 package com.example.eatmate.app.domain.member.controller;
 
-import com.example.eatmate.app.domain.member.dto.MyInfoResponseDto;
-import com.example.eatmate.app.domain.member.dto.MyInfoUpdateRequestDto;
-import com.example.eatmate.global.response.GlobalResponseDto;
-import jakarta.validation.Valid;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.eatmate.app.domain.member.dto.MemberLoginRequestDto;
 import com.example.eatmate.app.domain.member.dto.MemberLoginResponseDto;
+import com.example.eatmate.app.domain.member.dto.MyInfoResponseDto;
+import com.example.eatmate.app.domain.member.dto.MyInfoUpdateRequestDto;
 import com.example.eatmate.app.domain.member.service.MemberService;
+import com.example.eatmate.global.response.GlobalResponseDto;
 
 import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -35,21 +39,22 @@ public class MemberController {
 
 	@GetMapping("/myinfo")
 	@Operation(summary = "본인 정보 조회", description = "마이페이지에 들어올 시 조회되는 본인의 정보")
-	public ResponseEntity<GlobalResponseDto<MyInfoResponseDto>> getMyInfo(@AuthenticationPrincipal UserDetails userDetails) {
+	public ResponseEntity<GlobalResponseDto<MyInfoResponseDto>> getMyInfo(
+		@AuthenticationPrincipal UserDetails userDetails) {
 		// 서비스 호출 및 응답 반환
 		return ResponseEntity.ok(
-				GlobalResponseDto.success(memberService.getMyInfo(userDetails), HttpStatus.OK.value())
+			GlobalResponseDto.success(memberService.getMyInfo(userDetails))
 		);
 	}
 
 	@PatchMapping("/myinfo")
 	@Operation(summary = "프로필 수정", description = "사용자의 닉네임, 전화번호, MBTI, 생년월일을 일부 수정합니다.")
 	public ResponseEntity<GlobalResponseDto<MyInfoResponseDto>> updateMyProfile(
-			@RequestBody @Valid MyInfoUpdateRequestDto updateRequestDto,
-			@AuthenticationPrincipal UserDetails userDetails
+		@RequestBody @Valid MyInfoUpdateRequestDto updateRequestDto,
+		@AuthenticationPrincipal UserDetails userDetails
 	) {
 		return ResponseEntity.ok(
-				GlobalResponseDto.success(memberService.updateMyInfo(userDetails, updateRequestDto), HttpStatus.OK.value())
+			GlobalResponseDto.success(memberService.updateMyInfo(userDetails, updateRequestDto))
 		);
 	}
 }
