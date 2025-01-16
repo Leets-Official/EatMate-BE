07c3039bbe3efd1def612b8efca651aadb062582
seokjun01 <suckjungod@naver.com>
2025-01-16 16:21:21 +0900

    [add] #14 닉네임 필드 유효성 어노테이션 추가

--- src/main/java/com/example/eatmate/app/domain/member/dto/MyInfoUpdateRequestDto.java
+++ src/main/java/com/example/eatmate/app/domain/member/dto/MyInfoUpdateRequestDto.java
@@ -1,17 +1,16 @@
 package com.example.eatmate.app.domain.member.dto;
 
-import com.example.eatmate.app.domain.member.domain.BirthDate;
 import com.example.eatmate.app.domain.member.domain.Mbti;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
+
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 
 @Getter
-@Builder
-@AllArgsConstructor
 public class MyInfoUpdateRequestDto {
-    private String nickname;
-    private Mbti mbti;
 
+	@Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,12}$", message = "닉네임은 한글, 영문, 숫자로 이루어진 2~12자여야 하며 공백이 없어야 합니다.")
+	private String nickname;
+
+	private Mbti mbti;
 
 }
