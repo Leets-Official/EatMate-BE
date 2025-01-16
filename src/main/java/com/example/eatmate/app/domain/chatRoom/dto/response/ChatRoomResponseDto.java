5bb9c21c83c5d2df920abd1f9c43e54792db3af7
Dongyun Kim <djskaldrjfj@naver.com>
2025-01-16 11:47:34 +0900

    [refactor] #7 채팅방 dto 간소화

--- src/main/java/com/example/eatmate/app/domain/chatRoom/dto/response/ChatRoomResponseDto.java
+++ src/main/java/com/example/eatmate/app/domain/chatRoom/dto/response/ChatRoomResponseDto.java
@@ -1,12 +1,10 @@
 package com.example.eatmate.app.domain.chatRoom.dto.response;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.data.domain.Page;
 
 import com.example.eatmate.app.domain.chat.dto.ChatMessageDto;
-import com.example.eatmate.app.domain.chatRoom.domain.ChatRoom;
 
 import lombok.Builder;
 import lombok.Getter;
@@ -24,12 +22,7 @@ public class ChatRoomResponseDto {
 		this.participants = participants;
 	}
 
-	public static ChatRoomResponseDto from(ChatRoom chatRoom, Page<ChatMessageDto> chatPage) {
-		List<ChatMemberDto> participants = chatRoom.getParticipant() != null ? chatRoom.getParticipant()
-			.stream()
-			.map(memberChatRoom -> ChatMemberDto.from(memberChatRoom.getMember()))
-			.collect(Collectors.toList()) : null;
-
+	public static ChatRoomResponseDto of(List<ChatMemberDto> participants, Page<ChatMessageDto> chatPage) {
 		return ChatRoomResponseDto.builder()
 			.chats(chatPage)
 			.participants(participants)
