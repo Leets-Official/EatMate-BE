9ec7e424353fecf8b5e9f5c67d908af37d3adff0
EunHyunsu <ehs208@naver.com>
2025-01-18 18:38:38 +0900

    [rename] #37 파일명 및 디렉토리 변경

--- src/main/java/com/example/eatmate/global/common/util/StringToEnumConverterFactoryUtils.java
+++ src/main/java/com/example/eatmate/global/common/util/StringToEnumConverterFactoryUtils.java
@@ -1,11 +1,11 @@
-package com.example.eatmate.global.common;
+package com.example.eatmate.global.common.util;
 
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.core.convert.converter.ConverterFactory;
 import org.springframework.stereotype.Component;
 
 @Component
-public class StringToEnumConverterFactory implements ConverterFactory<String, Enum> {
+public class StringToEnumConverterFactoryUtils implements ConverterFactory<String, Enum> {
 
 	@Override
 	public <T extends Enum> Converter<String, T> getConverter(Class<T> targetType) {
--- src/main/java/com/example/eatmate/global/config/WebConfig.java
+++ src/main/java/com/example/eatmate/global/config/WebConfig.java
@@ -4,13 +4,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.example.eatmate.global.common.StringToEnumConverterFactory;
+import com.example.eatmate.global.common.util.StringToEnumConverterFactoryUtils;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
 	@Override
 	public void addFormatters(FormatterRegistry registry) {
-		registry.addConverterFactory(new StringToEnumConverterFactory());
+		registry.addConverterFactory(new StringToEnumConverterFactoryUtils());
 	}
 }
