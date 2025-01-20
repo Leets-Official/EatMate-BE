0b5048497e2f7524932f9fdb73e25adacd192d55
jj <syeonie0526@gmail.com>
2025-01-21 00:17:51 +0900

    [add] #77 차단 회원 목록 조회시 프로필 사진도 반환하도록 수정

--- src/main/java/com/example/eatmate/app/domain/block/dto/BlockMemberResponseDto.java
+++ src/main/java/com/example/eatmate/app/domain/block/dto/BlockMemberResponseDto.java
@@ -10,12 +10,15 @@ public class BlockMemberResponseDto {
 	private final Long blockId;
 	private final Long blockedMemberId;
 	private final String blockedUserNickname;
+	private final String profileImageUrl;
 
 	@Builder
-	private BlockMemberResponseDto(Long blockId, Long blockedMemberId, String blockedUserNickname) {
+	private BlockMemberResponseDto(Long blockId, Long blockedMemberId, String blockedUserNickname,
+		String profileImageUrl) {
 		this.blockId = blockId;
 		this.blockedMemberId = blockedMemberId;
 		this.blockedUserNickname = blockedUserNickname;
+		this.profileImageUrl = profileImageUrl;
 	}
 
 	public static BlockMemberResponseDto createBlockMemberResponseDto(Block block) {
@@ -23,6 +26,8 @@ public class BlockMemberResponseDto {
 			.blockId(block.getId())
 			.blockedMemberId(block.getBlockedMember().getMemberId())
 			.blockedUserNickname(block.getBlockedMember().getNickname())
+			.profileImageUrl(block.getBlockedMember().getProfileImage() != null ?
+				block.getBlockedMember().getProfileImage().getImageUrl() : null)
 			.build();
 	}
 }
