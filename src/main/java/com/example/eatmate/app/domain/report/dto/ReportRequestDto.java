39a873df330f5c0ab7c2a9c8331f534922901b79
jj <syeonie0526@gmail.com>
2025-01-26 01:52:21 +0900

    [refactor] #89 dto 검증 메세지 추가

--- src/main/java/com/example/eatmate/app/domain/report/dto/ReportRequestDto.java
+++ src/main/java/com/example/eatmate/app/domain/report/dto/ReportRequestDto.java
@@ -1,6 +1,7 @@
 package com.example.eatmate.app.domain.report.dto;
 
 import com.example.eatmate.app.domain.report.domain.ReportType;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
@@ -9,10 +10,11 @@ import lombok.Getter;
 @Getter
 public class ReportRequestDto {
 
-	@NotNull
+	@NotNull(message = "신고 유형이 비어있을 수 없습니다.")
+	@JsonProperty("ReportType")
 	private ReportType reportType;
 
-	@NotBlank
+	@NotBlank(message = "신고 사유는 비어있을 수 없습니다.")
 	private String reportingReasonDescription;    // 구체적인 신고 사유
 
 	@NotNull(message = "신고 대상이 비어있을 수 없습니다.")
