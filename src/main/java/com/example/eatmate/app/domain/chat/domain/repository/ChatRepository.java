3f7fea4f3b8f02a8b25be086757b14e45d7ae2b9
Dongyun Kim <djskaldrjfj@naver.com>
2025-01-28 16:41:58 +0900

    [refactor] #98 무한 스크롤 slice 방식으로 변경

--- src/main/java/com/example/eatmate/app/domain/chat/domain/repository/ChatRepository.java
+++ src/main/java/com/example/eatmate/app/domain/chat/domain/repository/ChatRepository.java
@@ -1,9 +1,10 @@
 package com.example.eatmate.app.domain.chat.domain.repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.eatmate.app.domain.chat.domain.Chat;
@@ -12,7 +13,9 @@ import com.example.eatmate.app.domain.chatRoom.domain.DeletedStatus;
 
 public interface ChatRepository extends JpaRepository<Chat, Long> {
 
-	Page<Chat> findChatByChatRoom(ChatRoom chatRoom, Pageable pageable);
+	Slice<Chat> findChatByChatRoomOrderByCreatedAtDesc(ChatRoom chatRoom, Pageable pageable);
+
+	Slice<Chat> findChatByChatRoomAndCreatedAtLessThanOrderByCreatedAtDesc(ChatRoom chatRoom, LocalDateTime createdAt, Pageable pageable);
 
 	List<Chat> findChatByChatRoomAndDeletedStatusNot(ChatRoom chatRoom, DeletedStatus deletedStatus);
 }
