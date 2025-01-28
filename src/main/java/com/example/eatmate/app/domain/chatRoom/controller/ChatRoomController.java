567887c7d7b772e20a35d934d4ef152a83495a8b
Dongyun Kim <djskaldrjfj@naver.com>
2025-01-28 16:42:31 +0900

    [refactor] #98 과거 채팅 로딩 api 분리

--- src/main/java/com/example/eatmate/app/domain/chatRoom/controller/ChatRoomController.java
+++ src/main/java/com/example/eatmate/app/domain/chatRoom/controller/ChatRoomController.java
@@ -1,8 +1,11 @@
 package com.example.eatmate.app.domain.chatRoom.controller;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
@@ -11,9 +14,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.eatmate.app.domain.chat.service.ChatPublisher;
+import com.example.eatmate.app.domain.chat.dto.response.ChatMessageListDto;
+import com.example.eatmate.app.domain.chat.service.ChatService;
 import com.example.eatmate.app.domain.chatRoom.dto.response.ChatRoomResponseDto;
 import com.example.eatmate.app.domain.chatRoom.service.ChatRoomService;
 import com.example.eatmate.global.response.GlobalResponseDto;
@@ -26,18 +31,29 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ChatRoomController {
 
-	private final ChatPublisher chatPublisher;
 	private final ChatRoomService chatRoomService;
+	private final ChatService chatService;
 
 	@GetMapping("/{chatRoomId}")
 	@Operation(summary = "채팅방 입장", description = "채팅방에 입장합니다.")
 	public ResponseEntity<GlobalResponseDto<ChatRoomResponseDto>> enterChatRoom(
 		@PathVariable Long chatRoomId,
 		@AuthenticationPrincipal UserDetails userDetails,
-		@PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.ASC) Pageable pageable) {
+		@PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
 
 		return ResponseEntity.status(HttpStatus.OK)
 			.body(GlobalResponseDto.success(chatRoomService.enterChatRoomAndLoadMessage(chatRoomId, userDetails, pageable)));
+	}
+
+	@GetMapping("/{chatRoomId}/past")
+	@Operation(summary = "채팅방의 과거 채팅 로딩", description = "채팅방의 과거 채팅을 로딩합니다.")
+	public ResponseEntity<GlobalResponseDto<ChatMessageListDto>> loadChatRoomPastMessage(
+		@PathVariable Long chatRoomId,
+		@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime cursor,
+		@PageableDefault(size = 20, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) {
+
+		return ResponseEntity.status(HttpStatus.OK)
+			.body(GlobalResponseDto.success(ChatMessageListDto.from(chatService.loadChat(chatRoomId, cursor, pageable))));
 	}
 
 	@PatchMapping("/{chatRoomId}")
