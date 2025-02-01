afd135dcebdbb58e8b2b51d393c32c9d78b98a97
seokjun01 <suckjungod@naver.com>
2025-02-02 02:52:39 +0900

    [mod]#115 mbti 소문자도 대문자로 입력되게끔 수정

--- src/main/java/com/example/eatmate/app/domain/member/dto/MemberSignUpRequestDto.java
+++ src/main/java/com/example/eatmate/app/domain/member/dto/MemberSignUpRequestDto.java
@@ -43,4 +43,8 @@ public class MemberSignUpRequestDto {
 	@Pattern(regexp = "^[가-힣a-zA-Z0-9]{2,12}$", message = "닉네임은 한글, 영문, 숫자로 이루어진 2~12자여야 하며 공백이 없어야 합니다.")
 	private String nickname;
 
+	public void setMbti(String mbti) {
+		this.mbti = mbti != null ? Mbti.valueOf(mbti.toUpperCase()) : null;
+	}
+
 }
