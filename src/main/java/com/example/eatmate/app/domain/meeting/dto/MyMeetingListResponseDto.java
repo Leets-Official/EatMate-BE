c833879d5d15e9a5d7075559aaabdd5b52213d5d
EunHyunsu <ehs208@naver.com>
2025-02-03 01:14:41 +0900

    [add] #155 모임 목록 조회시 필드값 추가

--- src/main/java/com/example/eatmate/app/domain/meeting/dto/MyMeetingListResponseDto.java
+++ src/main/java/com/example/eatmate/app/domain/meeting/dto/MyMeetingListResponseDto.java
@@ -3,6 +3,7 @@ package com.example.eatmate.app.domain.meeting.dto;
 import java.time.LocalDateTime;
 
 import com.example.eatmate.app.domain.meeting.domain.MeetingStatus;
+import com.example.eatmate.app.domain.meeting.domain.OfflineMeetingCategory;
 
 import lombok.Getter;
 
@@ -12,27 +13,37 @@ public class MyMeetingListResponseDto {
 	private Long id;
 	private String meetingName;
 	private MeetingStatus meetingStatus;
-	private Long participantCount;
+	private String meetingDescription;
+	private Long maxParticipants;
+	private OfflineMeetingCategory offlineMeetingCategory;
+	private LocalDateTime createdAt;
 	private String location;
 	private LocalDateTime dueDateTime;
+	private Long participantCount;
 
 	public MyMeetingListResponseDto(
-		String type,
+		String meetingType,
 		Long id,
 		String meetingName,
 		MeetingStatus meetingStatus,
+		String meetingDescription,
+		Long maxParticipants,
+		OfflineMeetingCategory offlineMeetingCategory,
+		LocalDateTime createdAt,
 		String location,
 		LocalDateTime dueDateTime,
-		Long participantCount
-	) {
-		this.meetingType = type;
+		Long participantCount) {
+		
+		this.meetingType = meetingType;
 		this.id = id;
 		this.meetingName = meetingName;
 		this.meetingStatus = meetingStatus;
+		this.meetingDescription = meetingDescription;
+		this.maxParticipants = maxParticipants;
+		this.offlineMeetingCategory = offlineMeetingCategory;
+		this.createdAt = createdAt;
 		this.location = location;
 		this.dueDateTime = dueDateTime;
 		this.participantCount = participantCount;
 	}
-
 }
-
