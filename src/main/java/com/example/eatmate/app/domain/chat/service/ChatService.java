1f54af81c3f1dcf47ef6d40421ca0a9ec36d0e2a
Dongyun Kim <djskaldrjfj@naver.com>
2025-02-04 12:34:35 +0900

    [refactor] #113 채팅 전송 인증 수정

--- src/main/java/com/example/eatmate/app/domain/chat/service/ChatService.java
+++ src/main/java/com/example/eatmate/app/domain/chat/service/ChatService.java
@@ -1,5 +1,6 @@
 package com.example.eatmate.app.domain.chat.service;
 
+import java.security.Principal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -18,6 +19,7 @@ import com.example.eatmate.app.domain.chatRoom.domain.ChatRoom;
 import com.example.eatmate.app.domain.chatRoom.domain.DeletedStatus;
 import com.example.eatmate.app.domain.chatRoom.domain.repository.ChatRoomRepository;
 import com.example.eatmate.app.domain.member.domain.Member;
+import com.example.eatmate.app.domain.member.domain.repository.MemberRepository;
 import com.example.eatmate.global.common.util.SecurityUtils;
 import com.example.eatmate.global.config.error.ErrorCode;
 import com.example.eatmate.global.config.error.exception.CommonException;
@@ -32,18 +34,20 @@ public class ChatService {
 	private final ChatPublisher chatPublisher;
 	private final ChatRepository chatRepository;
 	private final ChatRoomRepository chatRoomRepository;
+	private final MemberRepository memberRepository;
 	private final SecurityUtils securityUtils;
 
-	public void sendChatMessage(ChatMessageRequestDto chatMessageDto, UserDetails userDetail) {
+	public void sendChatMessage(ChatMessageRequestDto chatMessageDto) {
 		chatPublisher.sendMessage(chatMessageDto.getChatRoomId(), chatMessageDto);
-		saveChat(chatMessageDto, userDetail);
+		saveChat(chatMessageDto);
 	}
 
 	//채팅 저장
-	public void saveChat(ChatMessageRequestDto chatMessageDto, UserDetails userDetails) {
+	public void saveChat(ChatMessageRequestDto chatMessageDto) {
 		ChatRoom chatRoom = chatRoomRepository.findByIdAndDeletedStatus(chatMessageDto.getChatRoomId(), DeletedStatus.NOT_DELETED)
 			.orElseThrow(() -> new CommonException(ErrorCode.CHATROOM_NOT_FOUND));
-		Member member = securityUtils.getMember(userDetails);
+		Member member = memberRepository.findById(chatMessageDto.getSenderId())
+			.orElseThrow(() -> new CommonException(ErrorCode.MEMBER_CHATROOM_NOT_FOUND));//error 변경
 
 		Chat chat = Chat.createChat(chatMessageDto.getContent(), member, chatRoom);
 		chatRepository.save(chat);
