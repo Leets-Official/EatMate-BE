06790325ee6454b2121350c519d1f3c0a37c9faf
seokjun01 <suckjungod@naver.com>
2025-03-02 03:32:18 +0900

    [add] #190 yaml에서 사용자 값 불러오는 파일 생성

---
+++ src/main/java/com/example/eatmate/global/auth/login/oauth/GoogleOAuth2Properties.java
@@ -0,0 +1,15 @@
+package com.example.eatmate.global.auth.login.oauth;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "spring.security.oauth2.client.registration.google")
+public class GoogleOAuth2Properties {
+	private String clientId;
+	private String clientSecret;
+	private String redirectUri;
+}
