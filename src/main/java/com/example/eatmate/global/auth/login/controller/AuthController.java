bfa077665d731c8fb684241f4db712bbd19948e0
seokjun01 <suckjungod@naver.com>
2025-03-02 03:34:27 +0900

    [add] #190 토큰 발급 요청 api생성

--- src/main/java/com/example/eatmate/global/auth/login/controller/AuthController.java
+++ src/main/java/com/example/eatmate/global/auth/login/controller/AuthController.java
@@ -7,14 +7,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.example.eatmate.app.domain.member.domain.repository.MemberRepository;
 import com.example.eatmate.app.domain.member.dto.MemberSignUpRequestDto;
 import com.example.eatmate.app.domain.member.service.MemberService;
 import com.example.eatmate.global.auth.jwt.JwtService;
+import com.example.eatmate.global.auth.login.dto.OAuthToken;
 import com.example.eatmate.global.auth.login.dto.UserLoginResponseDto;
+import com.example.eatmate.global.auth.login.oauth.GoogleOAuth2Service;
 import com.example.eatmate.global.auth.login.service.LoginService;
 import com.example.eatmate.global.response.GlobalResponseDto;
 
@@ -33,6 +37,8 @@ public class AuthController {
 	private final MemberService memberService;
 	private final JwtService jwtService;
 	private final LoginService loginService;
+	private final MemberRepository memberRepository;
+	private final GoogleOAuth2Service googleOAuth2Service;
 
 	@PostMapping("/signup")
 	@Operation(summary = "회원가입", description = "회원가입을 합니다.")
@@ -59,4 +65,25 @@ public class AuthController {
 
 		return ResponseEntity.ok(GlobalResponseDto.success(userInfo, HttpStatus.OK.value()));
 	}
+
+	@PostMapping("/google")
+	public ResponseEntity<?> googleOAuthLogin(@RequestParam("code") String code) {
+		if (code == null || code.isEmpty()) {
+			return ResponseEntity.badRequest().body("인가 코드가 없습니다.");
+		}
+
+		log.info("Google OAuth2 로그인 요청, 원본 인가코드: {}", code);
+
+		OAuthToken tokenResponse = googleOAuth2Service.getGoogleAccessToken(code);
+		String googleAccessToken = tokenResponse.getAccessToken(); // 정상적으로 접근 가능
+
+		log.info("Google Access Token 발급 완료: {}", googleAccessToken);
+
+		// Google 사용자 정보 가져오기
+		UserLoginResponseDto response = googleOAuth2Service.processGoogleUserLogin(googleAccessToken);
+
+		return ResponseEntity.ok().body(response);
+	}
+
 }
+
