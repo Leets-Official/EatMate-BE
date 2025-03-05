15c6384f0ce2cf13730c27819dfaa99f1900bf5e
seokjun01 <suckjungod@naver.com>
2025-03-05 11:09:46 +0900

    [mod] #190  파일명 변경

--- src/main/java/com/example/eatmate/global/auth/login/dto/OAuthTokenResponseDto.java
+++ src/main/java/com/example/eatmate/global/auth/login/dto/OAuthTokenResponseDto.java
@@ -5,7 +5,8 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 
 @Getter
-public class OAuthToken {
+public class OAuthTokenResponseDto {
+
 	@JsonProperty("access_token")
 	private String accessToken;
 
